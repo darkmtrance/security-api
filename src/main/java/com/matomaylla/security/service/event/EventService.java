c2c6d647c19a44d142cb79d60d8a41e29361714f
michael tomaylla <pulser2310@gmail.com>
2023-05-21 00:17:20 -0500

    textcolor

--- src/main/java/com/matomaylla/security/service/event/EventService.java
+++ src/main/java/com/matomaylla/security/service/event/EventService.java
@@ -18,14 +18,14 @@ public class EventService {
 
     public EventResponse save(EventRequest request){
         var event = Event.builder().title(request.getTitle()).startDate(request.getStart())
-                .endDate(request.getEnd()).backgroundColor(request.getBackgroundColor()).build();
+                .endDate(request.getEnd()).backgroundColor(request.getBackgroundColor()).textColor(request.getTextColor()).build();
         Event eventResultado = repository.save(event);
         return EventResponse.builder().id(eventResultado.getId()).title(eventResultado.getTitle())
-                .start(eventResultado.getStartDate()).end(eventResultado.getEndDate()).backgroundColor(eventResultado.getBackgroundColor()).build();
+                .start(eventResultado.getStartDate()).end(eventResultado.getEndDate()).backgroundColor(eventResultado.getBackgroundColor()).textColor(eventResultado.getTextColor()).build();
     }
 
     public List<EventResponse> listar(){
-        List<EventResponse> eventResultado = repository.findAll().stream().map(p-> EventResponse.builder().id(p.getId()).title(p.getTitle()).start(p.getStartDate()).end(p.getEndDate()).backgroundColor(p.getBackgroundColor()).build()).collect(Collectors.toList());
+        List<EventResponse> eventResultado = repository.findAll().stream().map(p-> EventResponse.builder().id(p.getId()).title(p.getTitle()).start(p.getStartDate()).end(p.getEndDate()).backgroundColor(p.getBackgroundColor()).textColor(p.getTextColor()).build()).collect(Collectors.toList());
        return eventResultado;
     }
 }
